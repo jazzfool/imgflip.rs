07137d1162ea00a3c9994286c2e126ae46535902
robo9k <robo9k@symlink.io>
2020-03-21 16:59:27 +0100

    Quick & dirty caption_image

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,35 @@ struct MemeTemplatesData {
 }
 
 #[derive(Debug, Deserialize)]
-struct Response {
-    success: bool,
-    data: MemeTemplatesData,
+#[serde(untagged)]
+enum Response<T> {
+    SuccessResponse {
+        success: bool,
+        data: T,
+    },
+    FailureResponse {
+        success: bool,
+        error_message: String,
+    },
+}
+
+struct ImageCaptionRequest {
+    template_id: String,
+    username: String,
+    password: String,
+    text0: String,
+    text1: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct CaptionImageResponse {
+	url: Url,
+	page_url: Url,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let memes: Response = reqwest::Client::new()
+    let memes: Response<MemeTemplatesData> = reqwest::Client::new()
         .get("https://api.imgflip.com/get_memes")
         .send()
         .await?
@@ -32,6 +53,29 @@ async fn main() -> Result<(), reqwest::Error> {
         .await?;
 
     println!("{:#?}", memes);
+
+    let meme_caption = ImageCaptionRequest {
+        template_id: "61580".into(),
+        username: "freeforall6".into(),
+        password: "nsfw1234".into(),
+        text0: "text0".into(),
+        text1: "text1".into(),
+    };
+    let meme: Response<CaptionImageResponse> = reqwest::Client::new()
+        .post("https://api.imgflip.com/caption_image")
+        .query(&[
+            ("template_id", meme_caption.template_id),
+            ("username", meme_caption.username),
+            ("password", meme_caption.password),
+            ("text0", meme_caption.text0),
+            ("text1", meme_caption.text1),
+        ])
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", meme);
 
     Ok(())
 }
