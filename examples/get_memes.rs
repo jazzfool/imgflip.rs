564c8a49d20e1686a870b2440a3d7ada0b4377a4
robo9k <robo9k@symlink.io>
2020-04-06 16:52:13 +0200

    Use getters for formatting in get_memes example

--- examples/get_memes.rs
+++ examples/get_memes.rs
@@ -3,7 +3,17 @@ use imgflip::Client;
 #[tokio::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let memes = Client::new().memes().await?;
-    println!("{:#?}", memes);
+    for meme in memes {
+        println!(
+            "\"{}\" ({}), {} boxes, {}x{} @ {}",
+            meme.name(),
+            meme.id(),
+            meme.box_count(),
+            meme.width(),
+            meme.height(),
+            meme.url()
+        );
+    }
 
     Ok(())
 }
